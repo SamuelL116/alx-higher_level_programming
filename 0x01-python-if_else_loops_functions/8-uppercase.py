9d82dab8ae54bf24986007cd8bfe4729c2d1d941
SamuelL116 <lutumbasamuel9@gmail.com>
2022-05-31 21:04:44 +0000

    8-uppercase.py indentation fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for string in str:
         if ord(string) >= 97 and ord(string) <= 122:
-	    string = chr(ord(string) - 32)
+            string = chr(ord(string) - 32)
         print("{}".format(string), end="")
     print("")
