156c7d755dcd28fa0987b46002a57af0fb577d70
Samuel <lutumbasamuel9@gmail.com>
2022-06-06 13:03:50 -0700

    5-no_c.py indentation fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     string = ""
     for a in my_string:
-        if a != 'c' and a != 'C':
+        if (a != 'c') and (a != 'C'):
             new_str += a
     return (string)
