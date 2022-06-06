6775bfc6bde377713b56f2ff94a1098b82724ca2
Samuel <lutumbasamuel9@gmail.com>
2022-06-06 13:56:04 -0700

    divisible_by_2.py syntax fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     multiples = []
     for a in range(len(my_list)):
-        if my_list(a) % 2 == 0:
+        if my_list[a] % 2 == 0:
             multiples.append(True)
         else:
             multiples.append(False)
