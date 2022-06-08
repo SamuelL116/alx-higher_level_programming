93ac45b889968d6483b6a31e71c96a161a009595
Samuel <lutumbasamuel9@gmail.com>
2022-06-08 13:45:40 -0700

    101-square_matrix_map.py pycodestyle fix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def sqaure_matrix_map(matrix=[]):
-    return (list(map(lambda a: list(map(lambda v: v**2, a)), matrix)))
+    return (list(map(lambda a: list(map(lambda v: v**2, a[:])), matrix)))
