b692fe5971b2e3f77901a1765426a4598295a0c2
Samuel <lutumbasamuel9@gmail.com>
2022-06-20 12:27:00 -0700

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pthon3
+
+def safe_print_division(a, b):
+    try:
+        div = a / b
+    except (TypeError, ValueError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div)
