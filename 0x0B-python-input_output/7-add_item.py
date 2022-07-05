e125d7bdc5727d02bb15bd2e03a4123bafaa0070
Samuel <lutumbasamuel9@gmail.com>
2022-07-05 10:37:24 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""Module for appends elements in a json file."""
+import json
 import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+"""dump: crea un objeto usando un json y lo guarda en un archivo .txt"""
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+"""load: lee un archivo y los datos json los convierte en objeto."""
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
+try:
+    container = load_from_json_file('add_item.json')
+except:
+    container = []
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+for i in range(1, len(sys.argv)):
+    container.append(sys.argv[i])
+save_to_json_file(container, 'add_item.json')
